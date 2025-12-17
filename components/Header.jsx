9b99de47d25df866449be7ee4e21d728e31aa996
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-12-17 01:06:47 +0100

    [UPD] Header fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,42 +5,54 @@ const Header = () => {
     <>
       {/* Top Header */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light mx-2">
-        <a className="navbar-brand" href="#">
-          OneMillion€uroWebsite
-        </a>
+        {/* Logo → Homepage */}
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img
+            src="/logo.png"
+            alt="One Million Euro Website Logo"
+            height="40"
+            className="d-inline-block align-top"
+          />
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
           aria-controls="navbarNavDropdown"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
               <Link className="nav-link" to="/">
                 Homepage
               </Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/buy">
                 Buy Pixel
               </Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/faq">
                 FAQ
               </Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/pixels">
                 Pixel List
               </Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/contact">
                 Contact me
